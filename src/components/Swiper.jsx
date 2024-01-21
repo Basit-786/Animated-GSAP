8882b26a50f75d5920ae4007e2b3b98dc6fab18a
Basit-786 <arsajid090@gmail.com>
2024-01-21 23:22:50 +0500

    bug and animations fixing

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -12,18 +12,21 @@ const Swiper = () => {
   gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
+    // let sectionsWidth =  sectionRef.current.offsetWidth ;
+    // let amountToScroll = sectionsWidth - window.innerWidth;
+
     const pin = gsap.fromTo(
       sectionRef.current,
       {
         x: 0,
       },
       {
-        x: "-170vw",
+        x: '-80%',
         ease: "none",
-        duration: 1,
+        duration: 3,
         scrollTrigger: {
           trigger: triggerRef.current,
-          start: "top 5%",
+          start: "top 0%",
           end: "2000 top",
           scrub: 0.6,
           pin: true,
@@ -37,103 +40,105 @@ const Swiper = () => {
   }, []);
 
   return (
+    <div
+      ref={triggerRef}
+      className="overflow-hidden grid place-items-center h-[100vh] w-[100%]"
+    >
       <div
-        ref={triggerRef}
-        className="overflow-hidden h-[100vh] w-[100%] pt-[1px] -top-[1px]"
+        id="sections"
+        ref={sectionRef}
+        className="flex items-center relative"
       >
-        {/* <div className="flex items-center justify-center w-full h-full"> */}
-          <div id="sections" ref={sectionRef} className="flex items-center relative">
-            <div className="flex items-center justify-center gap-16 flex-shrink-0 flex-grow ml-[8rem]">
-              <div className="w-[400px] flex-shrink-0 relative">
-                <img
-                  src="/1.jpeg"
-                  alt=""
-                  className="w-full h-full object-cover object-center"
-                />
-              </div>
-              <div className="flex flex-col ml-[4rem] gap-[20px] flex-shrink-0 h-full max-w-[500px] text-lightBlack mb-8">
-                <h2 className="text-[45px] leading-[55px] uppercase font-normal">
-                  Take a luxurious trip through our extensive brand portfolio
-                </h2>
-                <p className="text-[#313434CC] text-[20px]">
-                  DIeteren Luxury Performance is the company of choice for the
-                  most prestigious of brands. Thanks to our local dealers,
-                  browse our extensive range of vehicles and services designed
-                  to meet the exacting requirements and expectations of those
-                  who are passionate about luxury and performance.
-                </p>
-                <div>
-                  <Link
-                    href={"#"}
-                    className="text-[#BDA191] inline-block border-b border-[#BDA191] text-[16px] uppercase "
-                  >
-                    Find out more
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="flex items-center justify-center gap-16 flex-shrink-0 flex-grow ml-[8rem]">
-              <div className="w-[400px] flex-shrink-0 relative">
-                <img
-                  src="/1.jpeg"
-                  alt=""
-                  className="w-full h-full object-cover object-center"
-                />
-              </div>
-              <div className="flex flex-col ml-[4rem] gap-[20px] flex-shrink-0 h-full max-w-[500px] text-lightBlack mb-8">
-                <h2 className="text-[45px] leading-[55px] uppercase font-normal">
-                  Take a luxurious trip through our extensive brand portfolio
-                </h2>
-                <p className="text-[#313434CC] text-[20px]">
-                  DIeteren Luxury Performance is the company of choice for the
-                  most prestigious of brands. Thanks to our local dealers,
-                  browse our extensive range of vehicles and services designed
-                  to meet the exacting requirements and expectations of those
-                  who are passionate about luxury and performance.
-                </p>
-                <div>
-                  <Link
-                    href={"#"}
-                    className="text-[#BDA191] inline-block border-b border-[#BDA191] text-[16px] uppercase "
-                  >
-                    Find out more
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="flex items-center justify-center gap-16 flex-shrink-0 flex-grow ml-[8rem]">
-              <div className="w-[400px] flex-shrink-0 relative">
-                <img
-                  src="/1.jpeg"
-                  alt=""
-                  className="w-full h-full object-cover object-center"
-                />
-              </div>
-              <div className="flex flex-col ml-[4rem] gap-[20px] flex-shrink-0 h-full max-w-[500px] text-lightBlack mb-8">
-                <h2 className="text-[45px] leading-[55px] uppercase font-normal">
-                  Take a luxurious trip through our extensive brand portfolio
-                </h2>
-                <p className="text-[#313434CC] text-[20px]">
-                  DIeteren Luxury Performance is the company of choice for the
-                  most prestigious of brands. Thanks to our local dealers,
-                  browse our extensive range of vehicles and services designed
-                  to meet the exacting requirements and expectations of those
-                  who are passionate about luxury and performance.
-                </p>
-                <div>
-                  <Link
-                    href={"#"}
-                    className="text-[#BDA191] inline-block border-b border-[#BDA191] text-[16px] uppercase "
-                  >
-                    Find out more
-                  </Link>
-                </div>
-              </div>
-            </div>
-          
+        <div className="flex items-center justify-center gap-16 flex-shrink-0 flex-grow ml-[10rem]">
+          <div className="w-[500px] flex-shrink-0 relative">
+            <img
+              src="/1.jpeg"
+              alt=""
+              className="w-full h-full object-cover object-center"
+            />
           </div>
-        {/* </div> */}
+          <div className="flex flex-col ml-[4rem] gap-[20px] flex-shrink-0 h-full max-w-[500px] text-lightBlack mb-8">
+            <h2 className="text-[45px] leading-[55px] uppercase font-normal">
+              Take a luxurious trip through our extensive brand portfolio
+            </h2>
+            <p className="text-[#313434CC] text-[20px]">
+              DIeteren Luxury Performance is the company of choice for the most
+              prestigious of brands. Thanks to our local dealers, browse our
+              extensive range of vehicles and services designed to meet the
+              exacting requirements and expectations of those who are passionate
+              about luxury and performance.
+            </p>
+            <div>
+              <Link
+                href={"#"}
+                className="text-[#BDA191] inline-block border-b border-[#BDA191] text-[16px] uppercase "
+              >
+                Find out more
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-center gap-16 flex-shrink-0 flex-grow ml-[10rem]">
+          <div className="w-[500px] flex-shrink-0 relative">
+            <img
+              src="/1.jpeg"
+              alt=""
+              className="w-full h-full object-cover object-center"
+            />
+          </div>
+          <div className="flex flex-col ml-[4rem] gap-[20px] flex-shrink-0 h-full max-w-[500px] text-lightBlack mb-8">
+            <h2 className="text-[45px] leading-[55px] uppercase font-normal">
+              Take a luxurious trip through our extensive brand portfolio
+            </h2>
+            <p className="text-[#313434CC] text-[20px]">
+              DIeteren Luxury Performance is the company of choice for the most
+              prestigious of brands. Thanks to our local dealers, browse our
+              extensive range of vehicles and services designed to meet the
+              exacting requirements and expectations of those who are passionate
+              about luxury and performance.
+            </p>
+            <div>
+              <Link
+                href={"#"}
+                className="text-[#BDA191] inline-block border-b border-[#BDA191] text-[16px] uppercase "
+              >
+                Find out more
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-center gap-16 flex-shrink-0 flex-grow ml-[10rem]">
+          <div className="w-[500px] flex-shrink-0 relative">
+            <img
+              src="/1.jpeg"
+              alt=""
+              className="w-full h-full object-cover object-center"
+            />
+          </div>
+          <div className="flex flex-col ml-[4rem] gap-[20px] flex-shrink-0 h-full max-w-[500px] text-lightBlack mb-8">
+            <h2 className="text-[45px] leading-[55px] uppercase font-normal">
+              Take a luxurious trip through our extensive brand portfolio
+            </h2>
+            <p className="text-[#313434CC] text-[20px]">
+              DIeteren Luxury Performance is the company of choice for the most
+              prestigious of brands. Thanks to our local dealers, browse our
+              extensive range of vehicles and services designed to meet the
+              exacting requirements and expectations of those who are passionate
+              about luxury and performance.
+            </p>
+            <div>
+              <Link
+                href={"#"}
+                className="text-[#BDA191] inline-block border-b border-[#BDA191] text-[16px] uppercase "
+              >
+                Find out more
+              </Link>
+            </div>
+          </div>
+        </div>
+      
       </div>
+    </div>
   );
 };
 
